ae08565e9e82299b2e791ee39ed3320d629fa80e
Jan Blahout <jan.blahout@outlook.com>
2024-02-26 20:50:29 +0100

    create a new page with games by genre

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import Layout from './components/Layout.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import GamesByTag from './components/GamesByTag.tsx';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
+import GamesByGenre from './components/GamesByGenre.tsx';
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,11 @@ const router = createBrowserRouter([
       {
         path: '/tags/:tag',
         element: <GamesByTag />,
+        errorElement: <ErrorBoundary />,
+      },
+      {
+        path: '/games/:genre',
+        element: <GamesByGenre />,
         errorElement: <ErrorBoundary />,
       },
     ],
